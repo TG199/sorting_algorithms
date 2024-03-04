c7a617327de5b8f7328a026bed49ce7538ab1264
Kelechi <ebirikele@gmail.com>
2024-03-04 22:43:58 +0100

    Selection sort, The quadratic algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort algorithm
+ * @array: array
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t temp, i, j, imin;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		imin = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[imin])
+				imin = j;
+			print_array(array, size);
+		}
+		temp = array[i];
+		array[i] = array[imin];
+		array[imin] = temp;
+	}
+}
